4b0061bafc36111aaede9a2272f88240e98aa755
AlonaKrupenko <88893788+AlonaKrupenko@users.noreply.github.com>
2021-11-17 20:51:20 +0200

    HW3

---
+++ HW3/script.js
@@ -0,0 +1,19 @@
+// Task #1
+let admin;
+let name;
+
+name = "Джон";
+
+admin = name;
+
+alert(admin);
+
+// Task #2
+let ourPlanetName = "Earth";
+let currentUserName = "Alona";
+
+// Task #3
+let number = "10" + 10;
+
+console.log(number, typeof number);
+// результат 1010 - при суммировании строк используется конкатенация, поэтому все сливается в единую строку
