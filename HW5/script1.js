7eaae0b6b805988a5046efd300790663384c7363
AlonaKrupenko <88893788+AlonaKrupenko@users.noreply.github.com>
2021-11-17 20:51:39 +0200

    HW5

---
+++ HW5/script1.js
@@ -0,0 +1,9 @@
+//Task #1
+
+const obj = {};
+obj.name = "John";
+obj.surname = "Smith";
+obj.name = "Pete";
+delete obj.name;
+
+console.log(obj);
---
+++ HW5/script3.js
@@ -0,0 +1,10 @@
+//Task #3
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+const sum = salaries.John + salaries.Ann + salaries.Pete;
+alert(sum);
