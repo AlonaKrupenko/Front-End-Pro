887f3d2f19ad28f0c5e8f7cd918d85e275f1f27f
AlonaKrupenko <88893788+AlonaKrupenko@users.noreply.github.com>
2021-11-18 18:52:00 +0200

    HW8

---
+++ HW8/script.js
@@ -0,0 +1,29 @@
+
+
+a === 0 ? alert("Верно") : alert("Неверно");
+a > 0 ? alert("Верно") : alert("Неверно");
+a < 0 ? alert("Верно") : alert("Неверно");
+a >= 0 ? alert("Верно") : alert("Неверно");
+a <= 0 ? alert("Верно") : alert("Неверно");
+a != 0 ? alert("Верно") : alert("Неверно");
+a === test ? alert("Верно") : alert("Неверно");
+a === "1" ? alert("Верно") : alert("Неверно");
+a > 0 && a < 5 ? alert("Верно") : alert("Неверно");
+a === 0 || a === 2 ? alert(a + 7) : alert(a / 10);
+a <= 1 && b >= 3 ? alert(a + b) : alert(a - b);
+(a > 2 && a < 11) || (b >= 6 && b < 14) ? alert("Верно") : alert("Неверно");
+
+switch (num) {
+  case 1:
+    season = "Зима";
+    break;
+  case 2:
+    season = "Весна";
+    break;
+  case 3:
+    season = "Лето";
+    break;
+  case 4:
+    season = "Осень";
+    break;
+}
